b409ce3c30ecb2ddc59d2a2d18690021983d4256
Abdullah yasin <abdullahyasinforwork@gmail.com>
2021-10-29 16:25:50 +0200

    add delete btn to every list item

--- app.js
+++ app.js
@@ -4,15 +4,29 @@ function addToList() {
     const inputContent = document.querySelector('#textfeild__input');
     // validate input
     if (inputContent.value.length >= 1) {
-        const listItem = document.createElement('p');
-        listItem.className = 'list__item';
-        listItem.innerHTML  = inputContent.value;
-        list.appendChild(listItem);
+
+        // crreate paragraph element as list item
+        const listParagrapgh = document.createElement('p');
+        listParagrapgh.className = 'list__paragrapgh';
+        listParagrapgh.innerHTML  = inputContent.value;
+
+        // biuld delete btn nested in the list item
+        const btn = document.createElement('button');
+        btn.innerHTML = "x";
+        btn.className = "list__paragrapgh__btn"
+        btn.addEventListener('click', () => {
+            btn.parentElement.remove();
+        });
+        listParagrapgh.appendChild(btn);
+        
+        // add the list item to the list container
+        list.appendChild(listParagrapgh);
         inputContent.value = '';
     } else {
         alert ('Field is empty, You must add task first!')
     }
 }
+
 
 // biuld clear all btn
 function clearALL() {
