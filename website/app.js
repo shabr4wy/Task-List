6c20942cd646af87a3c1e9d38ba185b560162510
Abdullah yasin <abdullahyasinforwork@gmail.com>
2021-11-18 09:33:53 +0200

    update done status

--- website/app.js
+++ website/app.js
@@ -70,7 +70,7 @@ function clearALL() {
         //first
         const cssRulesNum = document.styleSheets[0].cssRules.length;
         document.styleSheets[0].insertRule(`.list--animation1
-            { height: ${listHeight}px; transform: scale(1); transition: transform .5s, height 1s cubic-bezier(.49,.15,.34,.98) }`, cssRulesNum);
+            { height: ${listHeight}px; transform: scale(1); transition: transform .5s, height 1s`, cssRulesNum);
         list.classList.add('list--animation1');
         // second
         document.styleSheets[0].insertRule( `.list--animation2
